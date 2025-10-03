52e9e4cfe23dd60b45616fe602941e9e50dcb45d
Ludvig-D <74721111+Ludvig-D@users.noreply.github.com>
2025-10-03 12:08:56 +0200

    Store posts on reload and remove posts and made bottom div

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const postContainer = document.querySelector('.postContainer');
 
 const postForm = document.querySelector('.postForm').children[1];
 
+let posts = [];
+
+reload();
+
 function toggleForm() {
   formContainer.classList.toggle('hidden');
 }
@@ -17,36 +21,32 @@ toggleBtn.addEventListener('click', () => {
 
 cancelBtn.addEventListener('click', () => {
   toggleForm();
+  postForm[0].value = '';
+  postForm[1].value = '';
+  postForm[2].value = '';
 });
 
-function createPost() {
-  const title = postForm[0].value;
-  const username = postForm[1].value;
-  const text = postForm[2].value;
-
-  const date = new Date();
-  const postDate = `${date.getFullYear()}/${
-    date.getMonth() + 1
-  }/${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
-  console.log(postDate);
+function renderPost(post) {
+  const { title, username, text, id, date } = post;
 
   const postDiv = document.createElement('div');
   postDiv.classList.add('post');
+  postDiv.id = id;
 
-  const headerDiv = document.createElement('div');
-  headerDiv.classList.add('headerDiv');
+  const topDiv = document.createElement('div');
+  topDiv.classList.add('topDiv');
 
   // Title of post
   const titleH3 = document.createElement('h3');
   titleH3.innerText = title.charAt(0).toUpperCase() + title.slice(1);
-  headerDiv.appendChild(titleH3);
+  topDiv.appendChild(titleH3);
 
   // Date of post
   const dateP = document.createElement('p');
-  dateP.innerText = postDate;
-  headerDiv.appendChild(dateP);
+  dateP.innerText = date;
+  topDiv.appendChild(dateP);
 
-  postDiv.appendChild(headerDiv);
+  postDiv.appendChild(topDiv);
 
   // Name of creator
   const nameP = document.createElement('p');
@@ -58,11 +58,73 @@ function createPost() {
   textP.innerText = text;
   postDiv.appendChild(textP);
 
+  const bottomDiv = document.createElement('div');
+  bottomDiv.classList.add('bottomDiv');
+  postDiv.appendChild(bottomDiv);
+
+  const lcDiv = document.createElement('div');
+  bottomDiv.appendChild(lcDiv);
+
+  const likeBtn = document.createElement('button');
+  likeBtn.innerText = 'Like';
+  lcDiv.appendChild(likeBtn);
+
+  const dislikeBtn = document.createElement('button');
+  dislikeBtn.innerText = 'Dislike';
+  lcDiv.appendChild(dislikeBtn);
+
+  const commentBtn = document.createElement('button');
+  commentBtn.innerText = 'Comment';
+  lcDiv.appendChild(commentBtn);
+
+  const removeBtn = document.createElement('button');
+  removeBtn.innerText = 'Tabort';
+  bottomDiv.appendChild(removeBtn);
+
+  removeBtn.addEventListener('click', (e) => {
+    const postCon = e.target.parentElement.parentElement.parentElement;
+    const postDel = e.target.parentElement.parentElement;
+    const postId = e.target.parentElement.parentElement.id;
+    posts = posts.filter((post) => post.id !== postId);
+    savePosts();
+    postCon.removeChild(postDel);
+  });
+
   postContainer.appendChild(postDiv);
 }
 
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  createPost();
+  const post = createPostInfo();
+  savePosts();
+  renderPost(post);
   toggleForm();
 });
+
+async function savePosts() {
+  localStorage.setItem('posts', JSON.stringify(posts));
+}
+
+function reload() {
+  posts = JSON.parse(localStorage.getItem('posts'));
+  posts.map((post) => renderPost(post));
+}
+
+function createPostInfo() {
+  const date = new Date();
+  const min =
+    date.getMinutes() >= 10 ? date.getMinutes() : `0${date.getMinutes()}`;
+  const postDate = `${date.getFullYear()}/${
+    date.getMonth() + 1
+  }/${date.getDate()} ${date.getHours()}:${min}`;
+
+  const post = {
+    title: postForm[0].value,
+    username: postForm[1].value,
+    text: postForm[2].value,
+    id: crypto.randomUUID(),
+    date: postDate,
+  };
+  posts.push(post);
+  return post;
+}
